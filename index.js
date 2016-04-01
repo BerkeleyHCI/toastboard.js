0228b1cf37b99d6a13fa4fb634d0263472ea8b60
Julie Newcomb <newcombj@cs.washington.edu>
2016-03-31 21:14:50 -0700

    first stab at converting board comms from wifi to UART

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var WebSocket = require('ws');
+//var WebSocket = require('ws');
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
@@ -13,27 +13,32 @@ server.listen(port, function () {
 // Routing
 app.use(express.static(__dirname + '/public'));
 
-var ws = new WebSocket('ws://10.0.1.15');
+var serialport = require("serialport");
+var SerialPort = serialport.SerialPort;
 
-io.sockets.on('connection', function (socket) {
-    console.log('A new user connected!');
-      ws.on("open",function() {
-      console.log("websocket connected");
-    });
-
-    ws.on("message",function(data,flag) {
-      console.log("got data from websocket");
-      // when we get stuff from websocket, sling it over io socket to client
-      socket.emit('info', data);
-    });
-
-    socket.on("start",function(data,flag) {
-      console.log("clicked the get data button");
-      ws.send("start");
-    })
-
+var serialPort = new SerialPort("/dev/cu.usbserial-cc3101B",  {
+    baudrate: 9600,
+    parser: serialport.parsers.readline("\n")
 });
 
+io.sockets.on("connection",openSocket);
 
+function openSocket(socket) {
+  socket.on("start",function(data,flag) {
+    console.log("web client requested data");
+    serialPort.write("start\n", function(err,res) {
+      if (err !== undefined) {
+        console.log("error on writing to serial " + err);
+      }
+      if (res !== undefined) {
+        console.log("successfully wrote " + res + " characters");
+      }
+    });
+  });
 
-
+  serialPort.on("data", function(data) {
+    console.log("data from board");
+    console.log(data);
+    socket.emit("info",data);
+  })
+};
