8fe1be73089032aeaf2f84e1adf9d72fff0003dd
Julie Newcomb <newcombj@cs.washington.edu>
2016-04-02 16:47:25 -0700

    serial connection bug

--- index.js
+++ index.js
@@ -17,10 +17,63 @@ var serialport = require("serialport");
 var SerialPort = serialport.SerialPort;
 
 var serialPort = new SerialPort("/dev/cu.usbserial-cc3101B",  {
-    baudrate: 9600,
+  baudrate: 115200,
+//    baudrate: 9600,
     parser: serialport.parsers.readline("\n")
 }, false);
 
+io.sockets.on("connection",openSocket);
+
+function openSocket(socket) {
+  serialPort.open(function(error) {
+    if ( error ) {
+      console.log("no serial port");
+    } else {
+      socket.on("d",function(data,flag) {
+        console.log("web client requested one scan");
+        serialPort.write("d\n", function(err,res) {
+          if (err !== undefined) {
+            console.log("error on writing to serial " + err);
+          }
+          if (res !== undefined) {
+            console.log("successfully wrote " + res + " characters");
+          }
+        });
+      });
+
+      socket.on("s",function(data,flag) {
+        console.log("web client requested continuous scan");
+        serialPort.write("s\n", function(err,res) {
+          if (err !== undefined) {
+            console.log("error on writing to serial " + err);
+          }
+          if (res !== undefined) {
+            console.log("successfully wrote " + res + " characters");
+          }
+        });
+      });
+
+      socket.on("t",function(data,flag) {
+        console.log("web client requested stop continuous scan");
+        serialPort.write("t\n", function(err,res) {
+          if (err !== undefined) {
+            console.log("error on writing to serial " + err);
+          }
+          if (res !== undefined) {
+            console.log("successfully wrote " + res + " characters");
+          }
+        });
+      });
+
+      serialPort.on("data", function(data) {
+        console.log("data from board");
+        console.log(data);
+        socket.emit("info",data);
+      });
+    }
+  });
+};
+/*
 io.sockets.on("connection",openSocket);
 
 function openSocket(socket) {
@@ -72,3 +125,4 @@ function openSocket(socket) {
     }
   });
 };
+*/
