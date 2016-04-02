9b1f3a2315849159369a029f0d595ac2db10dd23
Julie Newcomb <newcombj@cs.washington.edu>
2016-04-01 23:19:50 -0700

    server stays up whether or not serial port is available

--- index.js
+++ index.js
@@ -15,30 +15,36 @@ app.use(express.static(__dirname + '/public'));
 
 var serialport = require("serialport");
 var SerialPort = serialport.SerialPort;
-/*
+
 var serialPort = new SerialPort("/dev/cu.usbserial-cc3101B",  {
     baudrate: 9600,
     parser: serialport.parsers.readline("\n")
-});
-*/
-// io.sockets.on("connection",openSocket);
+}, false);
+
+io.sockets.on("connection",openSocket);
 
 function openSocket(socket) {
-  socket.on("start",function(data,flag) {
-    console.log("web client requested data");
-    serialPort.write("start\n", function(err,res) {
-      if (err !== undefined) {
-        console.log("error on writing to serial " + err);
-      }
-      if (res !== undefined) {
-        console.log("successfully wrote " + res + " characters");
-      }
-    });
-  });
+  serialPort.open(function(error) {
+    if ( error ) {
+      console.log("no serial port");
+    } else {
+      socket.on("start",function(data,flag) {
+        console.log("web client requested data");
+        serialPort.write("start\n", function(err,res) {
+          if (err !== undefined) {
+            console.log("error on writing to serial " + err);
+          }
+          if (res !== undefined) {
+            console.log("successfully wrote " + res + " characters");
+          }
+        });
+      });
 
-  serialPort.on("data", function(data) {
-    console.log("data from board");
-    console.log(data);
-    socket.emit("info",data);
-  })
+      serialPort.on("data", function(data) {
+        console.log("data from board");
+        console.log(data);
+        socket.emit("info",data);
+      });
+    }
+  });
 };
