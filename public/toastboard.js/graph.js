4b3eb35d7156bad7605461ec0d6d85b79da4f7e5
Julie Newcomb <newcombj@cs.washington.edu>
2016-07-24 21:42:15 -0700

    better rounding on sampling rate

--- public/toastboard.js/graph.js
+++ public/toastboard.js/graph.js
@@ -21,7 +21,8 @@ Graph.prototype.addData = function(reading) {
   if (lastReading != undefined) {
     var lastTimestamp = lastReading.time[0];
     var msBetween = reading.time[0] - lastTimestamp;
-    hz = Math.floor(1 / (msBetween / 1000));
+    hz = 1 / (msBetween / 1000);
+    hz = +hz.toFixed(2);
   }
   this.sampleRate = hz;
   this.readings.push(reading.data); // probably not needed
